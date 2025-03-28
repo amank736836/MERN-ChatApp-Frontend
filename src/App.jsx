b7fe2954def3073f0e344bf8b8b8b0fba35169f6
Aman Kumar <amankarguwal0@gmail.com>
2025-03-28 23:14:14 +0530

    Add main application component with routing setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
