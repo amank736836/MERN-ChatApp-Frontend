e8be1b6f98b3d3a09b6a3c5c7522c2c987cd164a
Aman Kumar <amankarguwal0@gmail.com>
2025-03-29 00:40:37 +0530

    Implement lazy loading for routes and update routing structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { lazy } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+const Home = lazy(() => import("./pages/Home"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
+const Login = lazy(() => import("./pages/Login"));
 
 function App() {
   return (
     <Router>
+      {/* <span>Header</span> */}
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/about" element={<div>About</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
