9677f745468c47b57be5bb512e3d8f43046238fb
Aman Kumar <amankarguwal0@gmail.com>
2025-03-29 19:24:48 +0530

    Add LayoutLoader component for loading state visualization

---
+++ src/components/layout/Loaders.jsx
@@ -0,0 +1,43 @@
+import { Grid, Skeleton, Stack } from "@mui/material";
+
+export const LayoutLoader = () => {
+  return (
+    <Grid container height={"calc(100vh - 4rem)"} spacing={"1rem"}>
+      <Grid
+        size={{ sm: 4, md: 3 }}
+        sx={{ display: { xs: "none", sm: "block" } }}
+        height={"100%"}
+      >
+        <Skeleton
+          variant="rounded"
+          height={"100vh"}
+          sx={{
+            padding: "2rem",
+          }}
+        />
+      </Grid>
+      <Grid size={{ sm: 8, md: 5, lg: 6, xs: 12 }} height={"100%"} spacing={1}>
+        <Stack spacing={"1rem"}>
+          {Array.from({ length: 8 }, (_, index) => (
+            <Skeleton key={index} variant="rounded" height={95} sx={{}} />
+          ))}
+        </Stack>
+      </Grid>
+      <Grid
+        size={{ md: 4, lg: 3 }}
+        sx={{
+          display: { xs: "none", md: "block" },
+        }}
+        height={"100%"}
+      >
+        <Skeleton
+          variant="rounded"
+          height={"100vh"}
+          sx={{
+            padding: "2rem",
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
+};
