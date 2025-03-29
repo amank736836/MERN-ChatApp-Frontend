9a1f36a7680b5952311a0cec5ce6cd6234dbf981
Aman Kumar <amankarguwal0@gmail.com>
2025-03-29 19:24:25 +0530

    Implement route protection and lazy loading for application routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-import React, { lazy } from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
+import { LayoutLoader } from "./components/layout/Loaders";
 const Home = lazy(() => import("./pages/Home"));
 const Chat = lazy(() => import("./pages/Chat"));
 const Groups = lazy(() => import("./pages/Groups"));
 const Login = lazy(() => import("./pages/Login"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
+let user = true;
 
 function App() {
   return (
     <Router>
-      {/* <span>Header</span> */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat/:chatId" element={<Chat />} />
-        <Route path="/groups" element={<Groups />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Suspense fallback={<LayoutLoader />}>
+        {/* <span>Header</span> */}
+        <Routes>
+          <Route element={<ProtectRoute user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:chatId" element={<Chat />} />
+            <Route path="/groups" element={<Groups />} />
+          </Route>
+          <Route
+            path="/login"
+            element={
+              <ProtectRoute user={!user} redirect="/">
+                <Login />
+              </ProtectRoute>
+            }
+          />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
