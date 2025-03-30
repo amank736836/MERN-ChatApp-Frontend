b88780dcdb12ee53dfcc51dedffb95c82dd6ad7c
Aman Kumar <amankarguwal0@gmail.com>
2025-03-30 17:13:03 +0530

    Refactor AppLayout to use lazy loading for ChatList and Profile components; update chat handling logic

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,11 +1,23 @@
-import React from "react";
-import Header from "./Header";
 import { Grid } from "@mui/material";
+import React, { lazy } from "react";
+import { useParams } from "react-router-dom";
+import { sampleChats } from "../../constants/sampleData";
 import Title from "../shared/Title";
-import ChatList from "../../specific/ChatList";
+import Header from "./Header";
+
+const ChatList = lazy(() => import("../../specific/ChatList"));
+const Profile = lazy(() => import("../../specific/Profile"));
 
 const AppLayout = () => (WrappedComponent) => {
   return (props) => {
+    const params = useParams();
+    const chatId = params.chatId || "1";
+
+    const handleDeleteChat = (e, _id, groupChat) => {
+      e.preventDefault();
+      console.log("Delete chat", _id, groupChat);
+    };
+
     return (
       <>
         <Title title="Chatu App" description="This is the chat App" />
@@ -17,14 +29,20 @@ const AppLayout = () => (WrappedComponent) => {
             height={"100%"}
             bgcolor="primary.main"
           >
-            <ChatList chats={[1, 2, 3, 4, 5, 6, 7, 8]} chatId={1} />
+            <ChatList
+              chats={sampleChats}
+              chatId={chatId}
+              newMessagesAlert={[{ chatId, count: 2 }]}
+              onlineUsers={["1", "2"]}
+              handleDeleteChat={handleDeleteChat}
+            />
           </Grid>
           <Grid
             size={{ sm: 8, md: 5, lg: 6, xs: 12 }}
             height={"100%"}
             bgcolor="secondary.main"
           >
-            Main Content
+            <WrappedComponent {...props} />
           </Grid>
           <Grid
             size={{ md: 4, lg: 3 }}
@@ -35,10 +53,9 @@ const AppLayout = () => (WrappedComponent) => {
             }}
             height={"100%"}
           >
-            Right Sidebar
+            <Profile />
           </Grid>
         </Grid>
-        <WrappedComponent {...props} />
       </>
     );
   };
