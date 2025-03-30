cb7472b4f1c6b96f15cfe94c6568ec226be8603f
Aman Kumar <amankarguwal0@gmail.com>
2025-03-30 22:05:42 +0530

    Refactor Notifications component to use MUI Dialog and display friend requests with accept/reject options

--- src/specific/Notifications.jsx
+++ src/specific/Notifications.jsx
@@ -1,12 +1,93 @@
-import { Dialog } from '@mui/material'
-import React from 'react'
+import {
+  Avatar,
+  Button,
+  Dialog,
+  DialogTitle,
+  ListItem,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React, { memo } from "react";
+import { sampleNotifications } from "../constants/sampleData";
 
-const Notifications = () => {
+const Notifications = ({ close }) => {
+  const friendRequestHandler = ({ _id, accept }) => {};
+
   return (
-    <Dialog>
-      
-    </Dialog>
-  )
-}
+    <Dialog open onClose={close}>
+      <Stack
+        p={{
+          xs: "1rem",
+          sm: "2rem",
+        }}
+        maxWidth={"25rem"}
+      >
+        <DialogTitle>Notifications</DialogTitle>
 
-export default Notifications
+        {sampleNotifications.length > 0 ? (
+          sampleNotifications.map((notification) => (
+            <NotificationItem
+              sender={notification.sender}
+              _id={notification._id}
+              handler={friendRequestHandler}
+              key={notification._id}
+            />
+          ))
+        ) : (
+          <Typography textAlign={"center"}>No Notifications yet</Typography>
+        )}
+      </Stack>
+    </Dialog>
+  );
+};
+
+const NotificationItem = memo(({ sender, _id, handler }) => {
+  const { name, avatar } = sender;
+
+  return (
+    <ListItem>
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        spacing={"1rem"}
+        width={"100%"}
+      >
+        <Avatar src={sender.avatar} />
+
+        <Typography
+          variant="body1"
+          sx={{
+            flexGrow: 1,
+            display: "-webkit-box",
+            WebkitLineClamp: 1,
+            WebkitBoxOrient: "vertical",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {name} sent you a friend request
+        </Typography>
+
+        <Stack
+          direction={{
+            xs: "column",
+            sm: "row",
+          }}
+          spacing={"1rem"}
+        >
+          <Button
+            onClick={() => handler({ _id, accept: true })}
+            color="primary"
+          >
+            Accept
+          </Button>
+          <Button onClick={() => handler({ _id, accept: false })} color="error">
+            Reject
+          </Button>
+        </Stack>
+      </Stack>
+    </ListItem>
+  );
+});
+
+export default Notifications;
