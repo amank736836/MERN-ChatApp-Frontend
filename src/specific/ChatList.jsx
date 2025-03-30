43b3af343dc9e7177e7783b49dff31c12558054f
Aman Kumar <amankarguwal0@gmail.com>
2025-03-30 17:14:15 +0530

    Integrate ChatItem component into ChatList for improved chat display and functionality

--- src/specific/ChatList.jsx
+++ src/specific/ChatList.jsx
@@ -1,5 +1,6 @@
 import { Stack } from "@mui/material";
 import React from "react";
+import ChatItem from "../components/shared/ChatItem";
 
 const ChatList = ({
   w = "100%",
@@ -16,8 +17,37 @@ const ChatList = ({
 }) => {
   return (
     <Stack width={w} direction={"column"}>
-      {chats.map((data) => {
-        return <div>{data}</div>;
+      {chats.map((data, index) => {
+        const {
+          _id,
+          avatar,
+          name,
+          groupChat,
+          members = [],
+        } = data;
+
+        const newMessageAlert = newMessagesAlert.find(
+          ({ chatId }) => chatId === _id
+        );
+
+        const isOnline = members?.some(({ member }) =>
+          onlineUsers.includes(_id)
+        );
+
+        return (
+          <ChatItem
+            newMessageAlert={newMessageAlert}
+            isOnline={isOnline}
+            avatar={avatar}
+            name={name}
+            _id={_id}
+            key={_id}
+            groupChat={groupChat}
+            sameSender={_id === chatId}
+            index={index}
+            handleDeleteChat={handleDeleteChat}
+          />
+        );
       })}
     </Stack>
   );
