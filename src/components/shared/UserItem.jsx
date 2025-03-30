68a1e09494f76d23a43b03dcaff9a2cee3d73a2d
Aman Kumar <amankarguwal0@gmail.com>
2025-03-30 17:13:36 +0530

    Add UserItem component for displaying user information with action button

---
+++ src/components/shared/UserItem.jsx
@@ -0,0 +1,50 @@
+import { Add as AddIcon } from "@mui/icons-material";
+import { Avatar, IconButton, ListItem, Stack, Typography } from "@mui/material";
+import React, { memo } from "react";
+
+const UserItem = ({ user, handler, handlerIsLoading }) => {
+  const { _id, name, avatar } = user;
+
+  return (
+    <ListItem>
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        spacing={"1rem"}
+        width={"100%"}
+      >
+        <Avatar />
+        <Typography
+          variant="body1"
+          sx={{
+            flexGrow: 1,
+            display: "-webkit-box",
+            WebkitLineClamp: 1,
+            WebkitBoxOrient: "vertical",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {name}
+        </Typography>
+
+        <IconButton
+          onClick={() => handler(_id)}
+          disabled={handlerIsLoading}
+          size="small"
+          sx={{
+            bgcolor: "primary.main",
+            color: "white",
+            "&:hover": {
+              bgcolor: "primary.dark",
+            },
+          }}
+        >
+          <AddIcon />
+        </IconButton>
+      </Stack>
+    </ListItem>
+  );
+};
+
+export default memo(UserItem);
