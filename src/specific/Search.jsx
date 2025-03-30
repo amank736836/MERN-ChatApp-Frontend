435a04259c40ddccea98dc681db3333e959c78e2
Aman Kumar <amankarguwal0@gmail.com>
2025-03-30 17:14:27 +0530

    Implement search functionality with user list and friend request handler

--- src/specific/Search.jsx
+++ src/specific/Search.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useInputValidation } from "6pp";
+import { Search as SearchIcon } from "@mui/icons-material";
+import {
+  Dialog,
+  DialogTitle,
+  InputAdornment,
+  List,
+  Stack,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
+import UserItem from "../components/shared/UserItem";
+import { sampleUsers } from "../constants/sampleData";
 
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+  const search = useInputValidation("");
 
-export default Search
+  const [isLoadingSendFrindRequest, setIsLoadingSendFrindRequest] =
+    useState(false);
+
+  const sendFriendRequestHandler = async (userId) => {};
+
+  const [users, setUsers] = useState(sampleUsers);
+
+  return (
+    <Dialog open>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find a friend or group</DialogTitle>
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          varient="outlined"
+          size="small"
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+
+        <List>
+          {users.map((user) => (
+            <UserItem
+              user={user}
+              key={user._id}
+              handlerIsLoading={isLoadingSendFrindRequest}
+              handler={sendFriendRequestHandler}
+            />
+          ))}
+        </List>
+      </Stack>
+    </Dialog>
+  );
+};
+
+export default Search;
