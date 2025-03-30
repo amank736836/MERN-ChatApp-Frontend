50cce5607d1b3b40793cb29617e2d4c60d14ee46
Aman Kumar <amankarguwal0@gmail.com>
2025-03-30 22:05:37 +0530

    Implement NewGroup component with user selection and group creation functionality

--- src/specific/NewGroup.jsx
+++ src/specific/NewGroup.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { sampleUsers } from "../constants/sampleData";
+import UserItem from "../components/shared/UserItem";
+import { useInputValidation } from "6pp";
+import { usernameValidator } from "../utils/validators";
 
-const NewGroup = () => {
+const NewGroup = ({ close }) => {
+  const groupName = useInputValidation("", usernameValidator);
+
+  const [members, setMembers] = useState(sampleUsers);
+  const [selectedMembers, setSelectedMembers] = useState([]);
+
+
+  const selectMemberHandler = (userId) => {
+    if (!userId) return;
+    setSelectedMembers((prev) =>
+      prev.includes(userId)
+        ? prev.filter((id) => id !== userId)
+        : [...prev, userId]
+    );
+    setMembers((prev) =>
+      prev.map((user) =>
+        user._id === userId ? { ...user, isSelected: !user.selected } : user
+      )
+    );
+  };
+
+  const submitHandler = () => {};
+
   return (
-    <div>NewGroup</div>
-  )
-}
+    <Dialog open onClose={close}>
+      <Stack
+        p={{
+          xs: "1rem",
+          sm: "3rem",
+        }}
+        width={"25rem"}
+        spacing={"1rem"}
+      >
+        <DialogTitle textAlign={"center"} variant="h4">
+          Create a new group
+        </DialogTitle>
 
-export default NewGroup
+        <TextField
+          value={groupName.value}
+          onChange={groupName.changeHandler}
+          label="Group Name"
+          variant="outlined"
+          size="small"
+        />
+
+        <Typography variant="body1">
+          Select members to add to the group
+        </Typography>
+
+        <Stack>
+          {members.map((user) => (
+            <UserItem
+              user={user}
+              key={user._id}
+              handler={selectMemberHandler}
+              isAdded={selectedMembers.includes(user._id)}
+            />
+          ))}
+        </Stack>
+
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Button
+            variant="contained"
+            color="error"
+            size="large"
+            onClick={close}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            disabled={groupName.error}
+            sx={{
+              bgcolor: "success.main",
+              "&:hover": {
+                bgcolor: "success.dark",
+              },
+              "&:disabled": {
+                bgcolor: "darkorchid",
+                color: "white",
+              },
+            }}
+            onClick={submitHandler}
+          >
+            Create Group
+          </Button>
+        </Stack>
+      </Stack>
+    </Dialog>
+  );
+};
+
+export default NewGroup;
