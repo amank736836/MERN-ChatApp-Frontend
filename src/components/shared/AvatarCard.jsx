16b36d6945383dcb56d1a9f0e18db8dbc3f47f04
Aman Kumar <amankarguwal0@gmail.com>
2025-03-30 23:50:28 +0530

    Enhance AvatarCard component with improved alignment, spacing, and hover effects for better visual appeal

--- src/components/shared/AvatarCard.jsx
+++ src/components/shared/AvatarCard.jsx
@@ -3,9 +3,14 @@ import React from "react";
 
 const AvatarCard = ({ avatar = [], max = 4 }) => {
   return (
-    <Stack direction={"row"} spacing={0.5}>
-      <AvatarGroup max={max}>
-        <Box width={"5rem"} height={"3rem"}>
+    <Stack direction={"row"} spacing={0.5} alignItems={"center"}>
+      <AvatarGroup
+        max={max}
+        sx={{
+          gap: "0.3rem",
+        }}
+      >
+        <Box width={"5rem"} height={"3rem"} display="flex" alignItems="center">
           {avatar.map((src, index) => {
             if (index >= max) return null;
             return (
@@ -20,6 +25,14 @@ const AvatarCard = ({ avatar = [], max = 4 }) => {
                   left: {
                     xs: `${index * 0.5}rem`,
                     sm: `${index}rem`,
+                  },
+                  border: "2px solid white",
+                  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+                  transition: "transform 0.3s ease-in-out",
+                  transform: "translateX(15%)",
+                  "&:hover": {
+                    zIndex: 1,
+                    transform: "scale(1.1)",
                   },
                 }}
               />
