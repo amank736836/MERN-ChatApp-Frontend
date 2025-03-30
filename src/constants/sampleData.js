19b7c08f153a855461b2d478ccb8862effe0d73c
Aman Kumar <amankarguwal0@gmail.com>
2025-03-30 17:13:50 +0530

    Add sample data for chats and users in sampleData.js

---
+++ src/constants/sampleData.js
@@ -0,0 +1,55 @@
+export const sampleChats = [
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "John Doe",
+    _id: "1",
+    groupChat: false,
+    members: [
+      { _id: "1", name: "John Doe" },
+      { _id: "2", name: "Jane Doe" },
+    ],
+  },
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "Jane Doe",
+    _id: "2",
+    groupChat: false,
+    members: ["1", "2"],
+  },
+  {
+    avatar: [
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+      "https://www.w3schools.com/howto/img_avatar.png",
+    ],
+    name: "John Smith",
+    _id: "3",
+    groupChat: true,
+    members: ["1", "2"],
+  },
+];
+
+export const sampleUsers = [
+  {
+    _id: "1",
+    name: "John Doe",
+    avatar: "https://www.w3schools.com/howto/img_avatar.png",
+  },
+  {
+    _id: "2",
+    name: "Jane Smith",
+    avatar: "https://via.placeholder.com/150",
+  },
+];
