0b11933bd19078610a9b8f7874e87b3725ff381f
Aman Kumar <amankarguwal0@gmail.com>
2025-04-01 22:19:46 +0530

    Add RenderAttachment component for handling various file types

---
+++ src/components/shared/RenderAttachment.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { transformImageUrl } from "../../lib/features";
+import { FileOpen as FileOpenIcon } from "@mui/icons-material";
+
+const RenderAttachment = (fileType, url) => {
+  switch (fileType) {
+    case "video":
+      return (
+        <video src={url} controls style={{ width: "100%" }} preload="auto" />
+      );
+    case "image":
+      return (
+        <img
+          src={transformImageUrl(url, 200)}
+          alt="attachment"
+          width={"200px"}
+          height={"150px"}
+          style={{ objectFit: "contain" }}
+        />
+      );
+    case "audio":
+      return <audio src={url} controls preload="auto" />;
+    default:
+      return <FileOpenIcon />;
+  }
+};
+
+export default RenderAttachment;
