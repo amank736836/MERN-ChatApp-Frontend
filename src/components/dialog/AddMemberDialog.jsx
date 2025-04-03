da24e156318533e9b782134875c56bcf1b9a0088
Aman Kumar <amankarguwal0@gmail.com>
2025-04-03 22:47:53 +0530

    Enhance AddMemberDialog with member selection and submission functionality

--- src/components/dialog/AddMemberDialog.jsx
+++ src/components/dialog/AddMemberDialog.jsx
@@ -1,26 +1,74 @@
-import { Dialog, DialogTitle, Stack, Typography } from "@mui/material";
-import React from "react";
+import { Button, Dialog, DialogTitle, Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { sampleUsers } from "../../constants/sampleData";
 import UserItem from "../shared/UserItem";
 
 const AddMemberDialog = ({ addMember, isLoadingAddMember, chatId }) => {
-  const addFriendHandler = (id) => {
-    addMember(id, chatId);
+  const [members, setMembers] = useState(sampleUsers);
+  const [selectedMembers, setSelectedMembers] = useState([]);
+
+  const addMemberSubmitHandler = () => {
+    closeHandler();
   };
+
+  const closeHandler = () => {
+    setMembers(sampleUsers);
+    setSelectedMembers([]);
+  };
+
+  const selectMemberHandler = (userId) => {
+    if (!userId) return;
+    setSelectedMembers((prev) =>
+      prev.includes(userId)
+        ? prev.filter((id) => id !== userId)
+        : [...prev, userId]
+    );
+    setMembers((prev) =>
+      prev.map((user) =>
+        user._id === userId ? { ...user, isAdded: !user.isAdded } : user
+      )
+    );
+  };
+
+  useEffect(() => {
+    setMembers((prev) => prev.map((user) => ({ ...user, isAdded: false })));
+    setSelectedMembers([]);
+  }, []);
+
   return (
-    <Dialog open>
+    <Dialog open onClose={closeHandler}>
       <Stack p={"2rem"} width={"20rem"} spacing={"2rem"}>
-        <DialogTitle
-        textAlign={"center"}
-        >Add Member</DialogTitle>
+        <DialogTitle textAlign={"center"}>Add Member</DialogTitle>
         <Stack spacing={"1rem"}>
-          {sampleUsers.length > 0 ? (
-            sampleUsers.map((user) => (
-              <UserItem user={user} key={user.id} handler={addFriendHandler} />
+          {members.length > 0 ? (
+            members.map((user) => (
+              <UserItem
+                user={user}
+                key={user.id}
+                handler={selectMemberHandler}
+                isAdded={Boolean(user.isAdded)}
+              />
             ))
           ) : (
             <Typography textAlign={"center"}>No Known Found</Typography>
           )}
+        </Stack>
+        <Stack
+          direction={"row"}
+          justifyContent={"space-evenly"}
+          alignItems={"center"}
+        >
+          <Button color="error" variant="contained" onClick={closeHandler}>
+            Cancel
+          </Button>
+          <Button
+            color="success"
+            variant="contained"
+            disabled={isLoadingAddMember}
+            onClick={addMemberSubmitHandler}
+          >
+            Submit Changes
+          </Button>
         </Stack>
       </Stack>
     </Dialog>
