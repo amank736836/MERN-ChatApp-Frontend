8a18a3a256075e2fc2d545f9b2357455a401d851
Aman Kumar <amankarguwal0@gmail.com>
2025-04-05 19:24:25 +0530

    Add new background gradient constants for dialog and authentication

--- src/constants/color.jsx
+++ src/constants/color.jsx
@@ -3,3 +3,5 @@ export const grayColor = "rgba(247, 247, 247, 1)";
 export const lightBlue = "#2694ab";
 export const matBlack = "#1c1c1c";
 export const gradientBg = "linear-gradient(to bottom, #4facfe, #00f2fe)";
+export const dialogBg = "linear-gradient(135deg, #eef2f3, #8e9eab)";
+export const authBg = "linear-gradient(to right, #ff7e5f, #feb47b)";
