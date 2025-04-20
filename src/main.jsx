ece1ae306bb62fee049519f0ebfa586cfc5e4094
Aman Kumar <amankarguwal0@gmail.com>
2025-04-20 15:50:40 +0530

    Wrap application in Redux Provider for state management

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
-      <CssBaseline />
-      <div onContextMenu={(e) => e.preventDefault()}>
-        <App />
-      </div>
-    </HelmetProvider>
+    <Provider>
+      <HelmetProvider>
+        <CssBaseline />
+        <div onContextMenu={(e) => e.preventDefault()}>
+          <App />
+        </div>
+      </HelmetProvider>
+    </Provider>
   </StrictMode>
 );
