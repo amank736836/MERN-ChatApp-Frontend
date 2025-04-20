dfae9574c9b9c025a4ea434386901d305ae4f58a
Aman Kumar <amankarguwal0@gmail.com>
2025-04-20 17:45:39 +0530

    Fix Provider component to include Redux store for state management

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
+import store from "./redux/store.js";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider>
+    <Provider store={store}>
       <HelmetProvider>
         <CssBaseline />
         <div onContextMenu={(e) => e.preventDefault()}>
