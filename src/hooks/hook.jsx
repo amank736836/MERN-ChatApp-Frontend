a3e1f8a949b931e64eb7915b5f921c569023c547
Aman Kumar <amankarguwal0@gmail.com>
2025-04-25 21:47:22 +0530

    Add custom hooks for error handling and async mutations

---
+++ src/hooks/hook.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+const useErrors = (errors = []) => {
+  useEffect(() => {
+    errors.forEach(({ isError, error, fallback }) => {
+      if (isError) {
+        if (fallback) {
+          fallback();
+        }
+        if (error?.data?.message) {
+          toast.error(error.data.message, {
+            duration: 1000,
+          });
+        } else if (error?.message) {
+          toast.error(error.message, {
+            duration: 1000,
+          });
+        } else {
+          toast.error("Something went wrong", {
+            duration: 1000,
+          });
+        }
+      }
+    });
+  }, [errors]);
+};
+
+const useAsyncMutation = (mutationHook) => {
+  const [data, setData] = useState(null);
+
+  const [mutate, { isLoading, isError, error }] = mutationHook();
+
+  const executeMutation = async (toastMessage, ...args) => {
+    let toastId = toast.loading(toastMessage || "Updating data...");
+
+    try {
+      const res = await mutate(...args);
+
+      if (res.data) {
+        setData(res.data);
+        toast.success(res.data.message || "Updated Data Successfully", {
+          duration: 1000,
+          id: toastId,
+        });
+      } else if (res.error) {
+        toast.error(res?.error?.data?.message, {
+          duration: 1000,
+          id: toastId,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Error updating data", {
+        duration: 1000,
+        id: toastId,
+      });
+    }
+  };
+
+  return [
+    executeMutation,
+    {
+      data,
+      isLoading,
+      isError,
+      error,
+    },
+  ];
+};
+
+export { useErrors, useAsyncMutation };
