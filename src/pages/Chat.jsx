8e01cde9a0e946860a2bba5e2b87d229e9ceccb6
Aman Kumar <amankarguwal0@gmail.com>
2025-04-28 07:10:30 +0530

    Refactor Chat component to integrate socket events and improve message handling

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,25 +1,130 @@
-import React, { Fragment, useRef } from "react";
-import AppLayout from "../components/layout/AppLayout";
-import { grayColor, orange } from "../constants/color";
-import { IconButton, Stack } from "@mui/material";
+import { useInfiniteScrollTop } from "6pp";
 import {
   AttachFile as AttachFileIcon,
   Send as SendIcon,
 } from "@mui/icons-material";
-import { InputBox } from "../components/styles/StyledComponents";
+import { IconButton, Stack } from "@mui/material";
+import React, { Fragment, useCallback, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import FileMenu from "../components/dialog/FileMenu";
-import { sampleMessages } from "../constants/sampleData";
+import AppLayout from "../components/layout/AppLayout";
 import MessageComponent from "../components/shared/MessageComponent";
+import { InputBox } from "../components/styles/StyledComponents";
+import { grayColor, orange } from "../constants/color";
+import { NEW_MESSAGE } from "../constants/events";
+import { useErrors, useSocketEvents } from "../hooks/hook";
+import { useGetChatDetailsQuery, useGetMessagesQuery } from "../redux/api/api";
+import { setIsFileMenu } from "../redux/reducers/misc";
+import { getSocket } from "../socket";
 
-const user = {
-  _id: "sdmndnsad",
-  name: "John Doe",
-};
-
-const Chat = () => {
+const Chat = ({ chatId }) => {
+  const { user } = useSelector((state) => state.auth);
   const containerRef = useRef(null);
+  const socket = getSocket();
+  const dispatch = useDispatch();
 
-  return (
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [fileMenuAnchor, setFileMenuAnchor] = useState(null);
+
+  const {
+    data: chatDetails,
+    isLoading: isLoadingChatDetails,
+    isError: isErrorChatDetails,
+    error: errorChatDetails,
+  } = useGetChatDetailsQuery({
+    chatId,
+    skip: !chatId,
+    populate: false,
+  });
+
+  const {
+    data: oldMessagesChunks,
+    isLoading: isLoadingMessages,
+    isError: isErrorOldMessages,
+    error: errorOldMessages,
+  } = useGetMessagesQuery({
+    chatId,
+    page,
+    skip: !chatId,
+  });
+
+  useErrors([
+    {
+      isError: isErrorChatDetails,
+      error: errorChatDetails,
+    },
+    {
+      isError: isErrorOldMessages,
+      error: errorOldMessages,
+    },
+  ]);
+
+  const { data: oldMessages, setData: setOldMessages } = useInfiniteScrollTop(
+    containerRef,
+    oldMessagesChunks?.totalPages,
+    page,
+    setPage,
+    oldMessagesChunks?.messages
+  );
+
+  const allMessages = [...oldMessages, ...messages];
+
+  const members = chatDetails?.chat.members;
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!message.trim()) return;
+    socket.emit(NEW_MESSAGE, {
+      message,
+      chatId,
+      members,
+    });
+    setMessage("");
+  };
+
+  const newMessagesHandler = useCallback((data) => {
+    console.log("New message received", data);
+    setMessages((prevMessages) => {
+      const newMessages = [...prevMessages, data.message];
+      if (containerRef.current) {
+        containerRef.current.scrollTop =
+          containerRef.current.scrollHeight - containerRef.current.clientHeight;
+      }
+      return newMessages;
+    });
+  }, []);
+
+  const eventHandler = { [NEW_MESSAGE]: newMessagesHandler };
+
+  useSocketEvents(socket, eventHandler);
+
+  // useEffect(() => {
+  //   socket.on(NEW_MESSAGE, newMessagesHandler);
+  //   return () => {
+  //     socket.off(NEW_MESSAGE, newMessagesHandler);
+  //   };
+  // }, [socket]);
+
+  const handleFileOpen = () => {
+    dispatch(setIsFileMenu(true));
+    setFileMenuAnchor(e.currentTarget);
+  };
+
+  return isLoadingChatDetails ? (
+    <Stack
+      sx={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: grayColor,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Loading...
+    </Stack>
+  ) : (
     <Fragment>
       <Stack
         ref={containerRef}
@@ -31,20 +136,12 @@ const Chat = () => {
         sx={{
           overflowX: "hidden",
           overflowY: "auto",
-          // "&::-webkit-scrollbar": {
-          //   width: "8px",
-          //   backgroundColor: grayColor,
-          // },
-          // "&::-webkit-scrollbar-thumb": {
-          //   backgroundColor: "#888",
-          //   borderRadius: "4px",
-          // },
-          // "&::-webkit-scrollbar-thumb:hover": {
-          //   backgroundColor: "#555",
-          // },
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
         }}
       >
-        {sampleMessages.map((message, index) => (
+        {allMessages.map((message, index) => (
           <MessageComponent message={message} key={index} user={user} />
         ))}
       </Stack>
@@ -52,25 +149,35 @@ const Chat = () => {
         style={{
           height: "10%",
         }}
+        onSubmit={submitHandler}
       >
         <Stack
           direction={"row"}
           height={"100%"}
           padding={"1rem"}
           alignItems={"center"}
-          position={"relative"}
         >
           <IconButton
+            onClick={handleFileOpen}
             sx={{
-              position: "absolute",
-              left: "1.5rem",
               rotate: "30deg",
             }}
           >
             <AttachFileIcon />
           </IconButton>
 
-          <InputBox placeholder="Type a message..." />
+          <InputBox
+            placeholder="Type a message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            sx={{
+              width: "100%",
+              padding: "1rem",
+              borderRadius: "8px",
+              backgroundColor: "white",
+              boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+            }}
+          />
 
           <IconButton
             type="submit"
@@ -90,7 +197,7 @@ const Chat = () => {
         </Stack>
       </form>
 
-      <FileMenu />
+      <FileMenu anchorE1={fileMenuAnchor} />
     </Fragment>
   );
 };
