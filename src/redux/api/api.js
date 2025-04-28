2ce04cc05f34cceb581d35b153fe37a1150addda
Aman Kumar <amankarguwal0@gmail.com>
2025-04-28 07:10:47 +0530

    Add Message tag type and implement getNotifications, acceptFriendRequest, getChatDetails, and getMessages endpoints

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -6,7 +6,7 @@ const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: `${server}`,
   }),
-  tagTypes: ["Chat", "User"],
+  tagTypes: ["Chat", "User", "Message"],
   endpoints: (builder) => ({
     myChats: builder.query({
       query: () => ({
@@ -33,6 +33,41 @@ const api = createApi({
       }),
       invalidatesTags: ["User"],
     }),
+    getNotifications: builder.query({
+      query: () => ({
+        url: "/user/notifications",
+        credentials: "include",
+      }),
+      keepUnusedDataFor: 0,
+    }),
+    acceptFriendRequest: builder.mutation({
+      query: (data) => ({
+        url: `/user/acceptRequest`,
+        body: data,
+        method: "PUT",
+        credentials: "include",
+      }),
+      invalidatesTags: ["Chat"],
+    }),
+    getChatDetails: builder.query({
+      query: ({ chatId, populate = false }) => ({
+        url: `/chat/${chatId}`,
+        params: { populate },
+        method: "GET",
+        credentials: "include",
+      }),
+      providesTags: ["Chat"],
+    }),
+    getMessages: builder.query({
+      query: ({ chatId, page = 1 }) => ({
+        url: `/chat/message/${chatId}`,
+        params: { page },
+        method: "GET",
+        credentials: "include",
+      }),
+      providesTags: ["Message"],
+      keepUnusedDataFor: 0,
+    }),
   }),
 });
 
@@ -42,4 +77,8 @@ export const {
   useMyChatsQuery,
   useLazySearchUserQuery,
   useSendFriendRequestMutation,
+  useGetNotificationsQuery,
+  useAcceptFriendRequestMutation,
+  useGetChatDetailsQuery,
+  useGetMessagesQuery,
 } = api;
