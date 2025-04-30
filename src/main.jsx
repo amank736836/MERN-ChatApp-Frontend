fc8a2d8cf0dabc73187c0bdc612e317a31e72a9c
Aman Kumar <amankarguwal0@gmail.com>
2025-04-30 16:08:31 +0530

    Comment out StrictMode in main rendering to simplify component tree

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,14 @@ import store from "./redux/store.js";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
-      <HelmetProvider>
-        <CssBaseline />
-        <div onContextMenu={(e) => e.preventDefault()}>
-          <App />
-        </div>
-      </HelmetProvider>
-    </Provider>
-  </StrictMode>
+  // <StrictMode>
+  <Provider store={store}>
+    <HelmetProvider>
+      <CssBaseline />
+      <div onContextMenu={(e) => e.preventDefault()}>
+        <App />
+      </div>
+    </HelmetProvider>
+  </Provider>
+  // </StrictMode>
 );
