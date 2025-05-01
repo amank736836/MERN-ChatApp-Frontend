fdd0895f572e2f99aaecf130f5dd974924c4c7c2
Aman Kumar <amankarguwal0@gmail.com>
2025-05-01 11:41:02 +0530

    Reorder import statements in chat.js for consistency

--- src/redux/reducers/chat.js
+++ src/redux/reducers/chat.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getOrSaveFromStorage } from "../../lib/features";
 import { NEW_MESSAGE_ALERT } from "../../constants/events";
+import { getOrSaveFromStorage } from "../../lib/features";
 
 const initialState = {
   notificationCount: 0,
