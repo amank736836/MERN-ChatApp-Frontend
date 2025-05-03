68c79242ba0c0244109c32a3879e97d6903839c9
Aman Kumar <amankarguwal0@gmail.com>
2025-05-03 18:51:08 +0530

    Refactor ConfirmDeleteDialog to manage state internally and improve delete handling

--- src/components/dialog/ConfirmDeleteDialog.jsx
+++ src/components/dialog/ConfirmDeleteDialog.jsx
@@ -6,11 +6,47 @@ import {
   DialogContentText,
   DialogTitle,
 } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useAsyncMutation, useErrors } from "../../hooks/hook";
+import { setIsDeleteMenu } from "../../redux/reducers/misc.reducer";
+import { useNavigate } from "react-router-dom";
+import { useDeleteChatMutation } from "../../redux/api/api";
 
-const confirmDeleteDialog = ({ open, close, deleteHandler }) => {
+const confirmDeleteDialog = ({ chatId }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isDeleteMenu } = useSelector((state) => state.misc);
+
+  const handleClose = () => {
+    dispatch(setIsDeleteMenu(false));
+  };
+
+  const [
+    deleteChatMutation,
+    {
+      data: deleteChatData,
+      isLoading: isLoadingDeleteChat,
+      isError: isErrorDeleteChat,
+      error: errorDeleteChat,
+    },
+  ] = useAsyncMutation(useDeleteChatMutation);
+
+  useErrors([
+    {
+      isError: isErrorDeleteChat,
+      error: errorDeleteChat,
+    },
+  ]);
+
+  const deleteHandler = () => {
+    deleteChatMutation("Deleting Group...", chatId);
+    navigate("/groups");
+    handleClose();
+  };
+
   return (
-    <Dialog open={open} onClose={close}>
+    <Dialog open={isDeleteMenu} onClose={handleClose}>
       <DialogTitle>Confirm Delete</DialogTitle>
       <DialogContent>
         <DialogContentText>
@@ -19,7 +55,7 @@ const confirmDeleteDialog = ({ open, close, deleteHandler }) => {
         </DialogContentText>
       </DialogContent>
       <DialogActions>
-        <Button color="success" onClick={close}>
+        <Button color="success" onClick={handleClose}>
           No
         </Button>
         <Button color="error" onClick={deleteHandler}>
