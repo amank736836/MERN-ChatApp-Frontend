b9aa5377a8ee3942536ce8f62000de65e190fcaf
Aman Kumar <amankarguwal0@gmail.com>
2025-05-04 17:21:37 +0530

    Add SpeedInsights component for performance monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./redux/store.js";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")).render(
       <CssBaseline />
       <div onContextMenu={(e) => e.preventDefault()}>
         <App />
+        <SpeedInsights />
       </div>
     </Provider>
   </StrictMode>
