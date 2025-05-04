3bd45e51cdf6c84db59752fa1c6795f74c891ef5
Aman Kumar <amankarguwal0@gmail.com>
2025-05-04 17:13:50 +0530

    Refactor main rendering: remove unused HelmetProvider component

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,17 @@
 import { CssBaseline } from "@mui/material";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./redux/store.js";
-import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <HelmetProvider>
-        <CssBaseline />
-        <div onContextMenu={(e) => e.preventDefault()}>
-          <App />
-        </div>
-      </HelmetProvider>
+      <CssBaseline />
+      <div onContextMenu={(e) => e.preventDefault()}>
+        <App />
+      </div>
     </Provider>
   </StrictMode>
 );
