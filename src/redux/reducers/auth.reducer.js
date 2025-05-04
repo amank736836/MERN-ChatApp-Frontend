cae824bc2bd3cdf173b26c55655f12f89e05ffb0
Aman Kumar <amankarguwal0@gmail.com>
2025-05-04 14:23:57 +0530

    Refactor auth reducer: enhance admin authentication handling with success and error toast notifications

--- src/redux/reducers/auth.reducer.js
+++ src/redux/reducers/auth.reducer.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
+import { adminLogin, adminLogout, getAdmin } from "../thunks/admin.thunk";
 
 const initialState = {
   user: null,
@@ -18,6 +20,47 @@ const authSlice = createSlice({
       state.user = null;
       state.loader = false;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(adminLogin.fulfilled, (state, action) => {
+        state.isAdmin = true;
+        state.loader = false;
+        toast.success(action.payload.message, {
+          duration: 1000,
+        });
+      })
+      .addCase(adminLogin.rejected, (state, action) => {
+        state.loader = false;
+        toast.error(action.error.message, {
+          duration: 1000,
+        });
+      })
+      .addCase(getAdmin.fulfilled, (state, action) => {
+        if (action.payload.admin) {
+          state.isAdmin = true;
+        }
+        state.loader = false;
+        toast.success(action.payload.message, {
+          duration: 1000,
+        });
+      })
+      .addCase(getAdmin.rejected, (state, action) => {
+        state.loader = false;
+      })
+      .addCase(adminLogout.fulfilled, (state, action) => {
+        state.isAdmin = false;
+        state.loader = false;
+        toast.success(action.payload.message, {
+          duration: 1000,
+        });
+      })
+      .addCase(adminLogout.rejected, (state, action) => {
+        state.loader = false;
+        toast.error(action.error.message, {
+          duration: 1000,
+        });
+      });
   },
 });
 
