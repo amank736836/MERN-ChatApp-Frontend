1529f0a8786b42e5f6e9e515aa90db9fe2bdf822
Aman Kumar <amankarguwal0@gmail.com>
2025-05-04 18:28:22 +0530

    Remove SpeedInsights component from main render

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,12 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./redux/store.js";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <CssBaseline />
       <div onContextMenu={(e) => e.preventDefault()}>
-        <SpeedInsights />
         <App />
       </div>
     </Provider>
