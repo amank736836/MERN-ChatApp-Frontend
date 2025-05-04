8f4743a0fa6be392c5aa481ca5714a0f423b325b
Aman Kumar <amankarguwal0@gmail.com>
2025-05-04 14:24:47 +0530

    Fix ChatList component: correct destructuring of member in online user check

--- src/specific/ChatList.jsx
+++ src/specific/ChatList.jsx
@@ -37,7 +37,7 @@ const ChatList = ({
           ({ chatId }) => chatId === _id
         );
 
-        const isOnline = members?.some(({ member }) =>
+        const isOnline = members?.some((member) =>
           onlineUsers.includes(member)
         );
 
