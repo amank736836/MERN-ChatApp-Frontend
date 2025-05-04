afa765a749806a9773309e8a5d5e5c13b2d27be6
Aman Kumar <amankarguwal0@gmail.com>
2025-05-04 14:24:03 +0530

    Add admin thunk actions: implement login, retrieval, and logout functionality with error handling

---
+++ src/redux/thunks/admin.thunk.js
@@ -0,0 +1,60 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { server } from "../../constants/config";
+import axios from "axios";
+
+const initialState = {
+  user: null,
+  isAdmin: false,
+  loader: true,
+};
+
+const adminLogin = createAsyncThunk("admin/login", async (secretKey) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  };
+  try {
+    const { data } = await axios.post(
+      `${server}/admin/verify`,
+      { secretKey },
+      config
+    );
+    return data;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+});
+
+const getAdmin = createAsyncThunk("admin/getAdmin", async () => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  };
+  try {
+    const { data } = await axios.get(`${server}/admin`, config);
+    return data;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+});
+
+const adminLogout = createAsyncThunk("admin/logout", async () => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  };
+  try {
+    const { data } = await axios.get(`${server}/admin/logout`, config);
+    return data;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+});
+
+export { adminLogin, getAdmin, adminLogout };
