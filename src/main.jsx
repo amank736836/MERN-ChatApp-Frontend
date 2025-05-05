3ce9b388f4142bab527a926ceeb64f3eee8eaa49
Aman Kumar <amankarguwal0@gmail.com>
2025-05-05 22:08:09 +0530

    Enable StrictMode in main.jsx for improved error handling and warnings

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,12 @@ import App from "./App.jsx";
 import store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
+  <StrictMode>
     <Provider store={store}>
       <CssBaseline />
       <div onContextMenu={(e) => e.preventDefault()}>
         <App />
       </div>
     </Provider>
-  // </StrictMode> 
+  </StrictMode> 
 );
