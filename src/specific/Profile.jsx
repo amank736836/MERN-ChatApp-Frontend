14d00bd822465242ac7ebc01288c6af5f9de16c9
Aman Kumar <amankarguwal0@gmail.com>
2025-05-06 07:13:36 +0530

    Add functionality to accept anonymous messages in Profile component

--- src/specific/Profile.jsx
+++ src/specific/Profile.jsx
@@ -5,13 +5,15 @@ import {
   Link as LinkIcon,
   AlternateEmail as UsernameIcon,
 } from "@mui/icons-material";
-import { Avatar, Box, Stack, Typography } from "@mui/material";
+import { Avatar, Box, Stack, Switch, Typography } from "@mui/material";
 import moment from "moment";
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { gradientBg } from "../constants/color";
 import { transformImageUrl } from "../lib/features";
 import toast from "react-hot-toast";
+import axios from "axios";
+import { server } from "../constants/config";
 
 const Profile = () => {
   const { user } = useSelector((state) => state.auth);
@@ -24,6 +26,49 @@ const Profile = () => {
     toast.success("Profile URL copied to clipboard!", {
       duration: 2000,
     });
+  };
+
+  const [isAcceptingMessages, setIsAcceptingMessages] = useState(
+    user?.isAcceptingMessages || false
+  );
+
+  const handleAcceptMessages = async () => {
+    setIsAcceptingMessages((prev) => !prev);
+    const toastId = toast.loading("Updating Accepting Messages...");
+    try {
+      const response = await axios.post(
+        `${server}/user/acceptMessages`,
+        {
+          isAcceptingMessages: !isAcceptingMessages,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+
+      if (response.data.success) {
+        toast.success(
+          `You are now ${
+            !isAcceptingMessages ? "accepting" : "not accepting"
+          } messages!`,
+          {
+            duration: 1000,
+            id: toastId,
+          }
+        );
+        setIsAcceptingMessages(!isAcceptingMessages);
+      } else {
+        toast.error("Failed to update accepting messages", {
+          duration: 1000,
+          id: toastId,
+        });
+      }
+    } catch (error) {
+      toast.error("Failed to update accepting messages", {
+        duration: 1000,
+        id: toastId,
+      });
+    }
   };
 
   return (
@@ -59,6 +104,31 @@ const Profile = () => {
           src={transformImageUrl(user?.avatar?.url)}
           alt="Profile Avatar"
         />
+        <Stack
+          spacing={1}
+          direction="row"
+          alignItems="center"
+          sx={{
+            width: "100%",
+            padding: "1rem",
+            borderRadius: "8px",
+            backgroundColor: "#f5f5f5",
+            boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onClick={handleAcceptMessages}
+        >
+          <Stack>
+            <Typography variant="body1" fontWeight={600}>
+              Accept Anonymous Messages
+            </Typography>
+            <Typography color="gray" variant="caption">
+              {isAcceptingMessages}
+            </Typography>
+          </Stack>
+          <Switch checked={isAcceptingMessages} color="primary" size="medium" />
+        </Stack>
         <ProfileCard heading="Name" text={user?.name} Icon={<FaceIcon />} />
         <ProfileCard
           heading="Username"
@@ -71,12 +141,14 @@ const Profile = () => {
           text={moment(user?.createdAt).fromNow(true)}
           Icon={<CalendarIcon />}
         />
-        <ProfileCard
-          heading="Profile URL"
-          text={profileUrl}
-          Icon={<LinkIcon />}
-          handler={copyToClipboard}
-        />
+        {isAcceptingMessages && (
+          <ProfileCard
+            heading="Profile URL"
+            text={profileUrl}
+            Icon={<LinkIcon />}
+            handler={copyToClipboard}
+          />
+        )}
       </Stack>
     </Box>
   );
