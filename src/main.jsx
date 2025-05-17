1e90de7280da784de82ee943a94b8cf4d9c4f33b
Aman Kumar <amankarguwal0@gmail.com>
2025-05-17 11:28:23 +0530

    Fix import path for SpeedInsights component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./redux/store.js";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
