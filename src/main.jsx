0c66b0193aeeb6eb89b930b3c0070412a8b34c55
Aman Kumar <amankarguwal0@gmail.com>
2025-05-17 11:26:35 +0530

    Add Analytics and SpeedInsights components for performance tracking

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./redux/store.js";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <CssBaseline />
+      <Analytics />
+      <SpeedInsights />
       <div onContextMenu={(e) => e.preventDefault()}>
         <App />
       </div>
